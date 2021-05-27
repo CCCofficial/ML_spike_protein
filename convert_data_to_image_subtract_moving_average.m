2564f7d66ab5439b6a0eab5cfbbb993c0548584a
Shangying Wang <shangying2002@hotmail.com>
2021-05-27 14:57:49 -0400

    Update convert_data_to_image_subtract_moving_average.m

--- convert_data_to_image_subtract_moving_average.m
+++ convert_data_to_image_subtract_moving_average.m
@@ -1,12 +1,13 @@
 clear all; close all; clc;
-
+%the directory names to the distance matrices of ifferent mutations. There are in total 12 mutations and this code preprocesses all of them with a for loop.
 dir_name={'distanceMtrx-N501Y/','distanceMtrx-Q498Y/','distanceMtrx-N501V/','distanceMtrx-Q493Y/','distanceMtrx-N501T/','distanceMtrx-E484K/','distanceMtrx-N501S/','distanceMtrx-Q493N/','distanceMtrx-Q498N/','distanceMtrx-Q498K/','distanceMtrx-N501D/','distanceMtrx-G502P/'};
+%the names of the 12 mutations
 save_name={'N501Y','Q498Y','N501V','Q493Y','N501T','E484K','N501S','Q493N','Q498N','Q498K','N501D','G502P'};
 %all figures subtract the initial crystal state distance
 
-for n=1:12
+for n=1:12 %loop through the 12 mutations
     readFiles=dir(strcat(dir_name{n},'*.dat'));
-    numfiles=8000;%length(readFiles);
+    numfiles=8000;%length(readFiles); % 8000  molecular  snapshots (20ps each)
     file_name = {readFiles.name};
     str  = sprintf('%s#', file_name{:});
     num  = sscanf(str, ['d-Mtrx-' save_name{n} '.ts.%d.dat#']);
@@ -14,16 +15,16 @@ for n=1:12
     file_name = file_name(index);
 
     
-    initial=file_name{1}; %data for t=0ns
-    initial_data=table2array(readtable(strcat(dir_name{n},initial)));
+    initial=file_name{1}; %data for t=0ps
+    initial_data=table2array(readtable(strcat(dir_name{n},initial)));%load data at t=0
     
     step=20; %moving average, average over self and previous 19 frames.
-    prev=zeros(size(initial_data,1),size(initial_data,2),step);
+    prev=zeros(size(initial_data,1),size(initial_data,2),step); %record all the data before doing the moving average
     
-    for id=2:step %fill in the previous 9 instances
+    for id=2:step %fill in the previous 19 instances
         %get the file name
         data_name=file_name{id};
-        prev(:,:,id-1)=table2array(readtable(strcat(dir_name{n},data_name)))-initial_data;
+        prev(:,:,id-1)=table2array(readtable(strcat(dir_name{n},data_name)))-initial_data;%record data that has the background (t=0) removed 
     end
     
     for id=(step+1):numfiles
@@ -36,6 +37,7 @@ for n=1:12
 %         minv(n)=min(minv(n),min(min(norm_data)));
         picname=[pwd '/pic_' save_name{n} '_subtract_ave/' num2str(id-1, '%05d') '.png'];
         imwrite(norm_data,picname)
+        %everytime when moving one time step, the matrix moves to the left and only the last column need to be filled with new data
         prev(:,:,1:(step-1))=prev(:,:,2:step);
     end
 %     disp(maxv(n));
